b84c1dec511b32dee346aa22fa1864a88d057188
polpolcharchar <jackfitterer876@gmail.com>
2025-07-28 12:42:34 -0700

    Rearrange player page

--- app/player/[playerTag]/page.tsx
+++ app/player/[playerTag]/page.tsx
@@ -4,10 +4,10 @@ import { handleDynamicPlayerTagPath } from '@/components/BrawlComponents/HandleD
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { modeLabelMap, modeLabels } from '@/lib/BrawlUtility/BrawlConstants';
+import { modeLabelMap } from '@/lib/BrawlUtility/BrawlConstants';
 import { fetchPlayerOverview } from '@/lib/BrawlUtility/BrawlDataFetcher';
 import { usePlayerData } from '@/lib/BrawlUtility/PlayerDataProvider';
-import { Check, CheckCircle, CheckSquare, MinusSquare, X, XCircle, XSquare } from 'lucide-react';
+import { CheckCircle, CheckSquare, MinusSquare, XCircle, XSquare } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
@@ -42,53 +42,49 @@ export default function UserPage() {
     playerData[activePlayerTag] ? (
       <div className="flex flex-1 flex-col gap-4 p-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-          <Card className="bg-muted/50 aspect-video rounded-xl text-center border-none">
-            <p className="text-6xl font-bold">{playerData[activePlayerTag].name}</p>
-            <p className="text-2xl text-gray-500">#{activePlayerTag}</p>
-          </Card>
-          <Card className="bg-muted/50 aspect-video rounded-xl text-left border-none pl-2 gap-2 text-2xl text-(--foreground) font-bold">
+          <Card className="bg-muted/50 rounded-xl text-center border-none">
+            <div>
+              <p className="text-6xl font-bold">{playerData[activePlayerTag].name}</p>
+              <p className="text-2xl text-gray-500">#{activePlayerTag}</p>
 
-            {/* Display brawler in title case */}
-            <p>
-              Favorite brawler:{" "}
-              {playerOverview?.favoriteBrawlers?.[0]?.brawler
-                ? playerOverview.favoriteBrawlers[0].brawler
-                  .toLowerCase()
-                  .split(" ")
-                  .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
-                  .join(" ")
-                : "N/A"}
-            </p>
-            <p>
-              {"Favorite mode: "}
-              {playerOverview?.favoriteModes?.[0]?.mode && modeLabelMap[playerOverview.favoriteModes[0].mode as keyof typeof modeLabelMap]}
-            </p>
-
-            <p>Last seen:</p>
-
-            <div className="flex items-center gap-1">
-              <p>BrawlBolt verified:</p>
-              {playerData[activePlayerTag]["verified"] ? (
-                <CheckCircle className='text-(--primary)' />
-              ) : (
-                <div className='flex gap-2 items-center'>
-                  <XCircle className='text-(--destructive)' />
-                  <Button className='text-(--foreground)' onClick={() => router.push("/verify")}>
-                    Claim Now
-                  </Button>
-                </div>
-              )}
             </div>
+            <div className='text-2xl text-(--foreground) font-bold items-center'>
 
+              {/* Display brawler in title case */}
+              <p>
+                Favorite brawler:{" "}
+                {playerOverview?.favoriteBrawlers?.[0]?.brawler
+                  ? playerOverview.favoriteBrawlers[0].brawler
+                    .toLowerCase()
+                    .split(" ")
+                    .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+                    .join(" ")
+                  : "N/A"}
+              </p>
+              <p>
+                {"Favorite mode: "}
+                {playerOverview?.favoriteModes?.[0]?.mode && modeLabelMap[playerOverview.favoriteModes[0].mode as keyof typeof modeLabelMap]}
+              </p>
+
+              <p>Last seen:</p>
+
+              <div className="flex items-center justify-center gap-1">
+                <p>BrawlBolt verified:</p>
+                {playerData[activePlayerTag]["verified"] ? (
+                  <CheckCircle className='text-(--primary)' />
+                ) : (
+                  <div className='flex gap-2 items-center'>
+                    <XCircle className='text-(--destructive)' />
+                    <Button className='text-(--foreground)' onClick={() => router.push("/verify")}>
+                      Claim Now
+                    </Button>
+                  </div>
+                )}
+              </div>
+            </div>
           </Card>
-          {/* <Card className="bg-muted/50 aspect-video rounded-xl text-center border-none">
-            Basic match history: last 5 games, win or loss, mode, rank, implement from backend
-          </Card> */}
-        </div>
 
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min flex flex-col items-center">
+          <div className="bg-muted/50 flex-1 rounded-xl md:min-h-min flex flex-col items-center">
             {playerOverview && (
               <div className='flex flex-col items-center'>
                 <p className='text-lg font-bold my-2'>Recent Games</p>
@@ -138,8 +134,9 @@ export default function UserPage() {
               </div>
             )}
           </div>
+        </div>
 
-
+        <div className="grid auto-rows-min gap-4 md:grid-cols-2">
           <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min flex flex-col items-center">
             {playerOverview && (
               <div className='flex flex-col items-center'>
